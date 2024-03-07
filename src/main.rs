28130a74ae64078021ca8bf6a3be602bb5b68905
Ben Smith <bh2smith@gmail.com>
2024-03-07 09:52:34 +0100

    add workflow and test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use reqwest::Response;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+async fn fetch() -> Result<Response, reqwest::Error> {
     let url = "https://metroverse.com/blocks/14623";
-    let response = reqwest::Client::new().get(url).send().await;
-    println!("Reqwest {response:?}");
-
+    // let client = reqwest::Client::new();
     let client = reqwest::Client::builder()
         .tls_built_in_root_certs(true)
         .build()
         .unwrap();
-    let x = client.get(url).send().await;
-    println!("Reqwest {x:?}");
+    let result = client.get(url).send().await;
+    println!("{result:?}");
+    result
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    fetch().await.context("failed fetch")?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn get_block() {
+        assert!(fetch().await.is_ok())
+    }
 }
